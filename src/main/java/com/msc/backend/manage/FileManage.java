77c9da9a69d50d39a7c58d99b699fa6f2a864196
RUIXIANG XIONG <2540725x@student.gla.ac.uk>
2021-06-21 15:37:07 +0100

    add MultipartFile class

--- src/main/java/com/msc/backend/manage/FileManage.java
+++ src/main/java/com/msc/backend/manage/FileManage.java
@@ -6,8 +6,10 @@ import com.msc.backend.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.msc.backend.config.GeneralConfig;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
